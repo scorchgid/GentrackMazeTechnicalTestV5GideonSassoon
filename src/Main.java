7e3be45d7461f857846a8f9cce8a7066bb1cceac
Gideon Sassoon <GSassoon@britishmuseum.org>
2019-03-28 13:27:25 +0000

    Added 1st steps - building list of files. Not tested as this stage so mahy be completely wrong.
    Methods
    buildFileList split to make it easy to call when needed.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
-public class Main {
+import.java.io.FileReader;
 
+public class Main {
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        //Test commit 2
+        List<File> files = buildFileList();        
+    }
+
+    public List<File> buildFileList(){
+        try {
+            List<File> files;
+            File folder;
+            for (final File file : folder.listFiles()) {
+                    files.add(file.getAbsoluteFile());
+                    System.out.println("File retrieved: " + file.getAbsoluteFile().toString());
+            }
+        } catch(Exception e){
+            System.out.println("Exception thrown " + e.toString());
+        }
+        return files;
     }
 }
